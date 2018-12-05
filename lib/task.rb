36f20dfa121f62d0ce36b76813774d38bf092e20
Brooke <perrysbrooke@gmail.com>
2018-12-04 23:14:58 -0800

    follow along with hw

---
+++ lib/task.rb
@@ -0,0 +1,33 @@
+class Task
+  attr_reader(:description, :list_id)
+
+  def initialize(attributes)
+    @descrition = attributes.fetch(:description)
+    @list_id = attributes.fetch(:list_id)
+  end
+
+  def self.all
+    returned_tasks = DB.exec("SELECT * FROM tasks;")
+    tasks =[]
+    returned_tasks.each() do |task|
+      description = task.fetch("description")
+      list_id = task.fetch("list_id").to_i
+      tasks.push.(Task.new({:description => description, :list_id => list_id}))
+    end
+    tasks
+  end
+
+  def ==(another_task)
+    self.description().==(another_task.description())
+  end
+
+  def save
+    DB.exec("INSERT INTO tasks (description) VALUES ('#{@description}', #{@list_id});")
+  end
+
+  def ==(another_task)
+    self.description().==(another_task.description()).&(self.list_id().==(another_task.list_id))
+
+  end
+
+end
